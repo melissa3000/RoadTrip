1ff84c173c67e81eda93e4839a5aa215e6134b73
Melissa Boyer <melissaboyer3000@gmail.com>
2017-02-13 17:26:36 +0000

    Changed search to nearby instead of radar and excluded search results closest to point of origin

--- static/map_results.js
+++ static/map_results.js
@@ -106,6 +106,349 @@
 
 // //------------------display directions----------------------------------
 
+ //Creates path overlay on map from start to end location, hard coded for stinson beach to petaluma
+//   function displayDirections() {
+
+//     var stinsonBeach = {lat:37.9005, lng: -122.6444};
+//     var petaluma = {lat: 38.2324, lng: -122.6367};
+
+//     var request = {
+//       origin: stinsonBeach, // start
+//       destination: petaluma, // end
+//       travelMode: 'DRIVING'
+//     };
+
+//     var directionsService = new google.maps.DirectionsService;
+//     directionsService.route(request, function(response, status) {
+//       if (status === google.maps.DirectionsStatus.OK) {
+//         directionsDisplay.setDirections(response);
+//       } else {
+//         window.alert('Directions request failed due to ' + status);
+//       }
+//     });
+
+//     var directionsDisplay = new google.maps.DirectionsRenderer;
+//     directionsDisplay.setMap(map);
+//   }
+
+//   displayDirections();
+
+// }
+//   //incorporate later when start and end are taken from DOM
+//   // var start = document.getElementById('start').value;
+//   // var end = document.getElementById('end').value;
+
+// initMap();
+
+
+
+
+
+
+
+// --------------Full function to return search results along path------------------------
+
+
+
+// function initialize() {
+//   // intial map hard coded to center at Oakland
+//   var oakland = {lat: 37.8044, lng: -122.2711};
+
+//   var mapOptions = {
+//     center: oakland,
+//     zoom: 8
+//   };
+
+//   var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+//   var service = new google.maps.places.PlacesService(map);
+
+//   //assigns variable routeBoxer as new object from RouteBoxer library
+//   var routeBoxer = new RouteBoxer();
+
+//   //directionsService makes a directions request from Google maps
+//   //directionsRenderer has Google maps render the directions it just returned
+//   var directionService = new google.maps.DirectionsService();
+//   var directionsRenderer = new google.maps.DirectionsRenderer({
+//     map: map
+//   });
+
+//   drawPath();
+
+//   var boxes;
+
+//   function drawPath() {
+
+//     var distance = 5.0; // sets search box size to 5km from route path
+
+//     //search parameters for draw path request: start point, end point, travel method
+//     var pathRequest = {
+//       origin: {lat: 37.8044, lng: -122.2711}, // hard coded to oakland for testing
+//       destination: {lat: 38.2324, lng: -122.6367}, // hard coded to petaluma for testing
+//       travelMode: google.maps.DirectionsTravelMode.DRIVING
+//     };
+
+//     // Make the directions request, if the status is ok, create a path and
+//     // routeBox search boxes along the path
+//     directionService.route(pathRequest, function(result, status) {
+//       if (status == google.maps.DirectionsStatus.OK) {
+//         directionsRenderer.setDirections(result);
+
+//         //path_overview smooths out the path coordinates
+//         var path = result.routes[0].overview_path;
+//         // creates search boxes around the returned path
+//         boxes = routeBoxer.box(path, distance);
+
+//         //turn on drawBoxes for testing if you want to visualize search boundaries,
+//         //function is commented out below
+//         // drawBoxes();
+
+//         //since direction request was successful,
+//         //call findPlaces function with searchIndex zero
+//         findPlaces(0);
+//       } else {
+//         alert("Directions query failed: " + status);
+//       }
+//     });
+
+//   }
+
+//   // Draw the array of boxes as polylines on the map - helpful to visualize search while testing
+//   // function drawBoxes() {
+//   //   var boxpolys = new Array(boxes.length);
+//   //   for (var i = 0; i < boxes.length; i++) {
+//   //     boxpolys[i] = new google.maps.Rectangle({
+//   //       bounds: boxes[i],
+//   //       fillOpacity: 0,
+//   //       strokeOpacity: 1.0,
+//   //       strokeColor: '#000000',
+//   //       strokeWeight: 1,
+//   //       map: map
+//   //     });
+//   //   }
+//   // }
+
+
+//   function findPlaces(searchIndex) {
+
+//     // search request is defined as the area bound by each routeBox box,
+//     // search type is hard coded as restaurant for testing
+//     var radarRequest = {
+//       bounds: boxes[searchIndex],
+//       type: 'restaurant'
+//     };
+
+//     //radarSearch allows a 'type' search within a given radius,
+//     // if search is successful, create a marker for each result.
+//     // If not, set Timeout to allow a delay and search again due to query limits
+//     service.radarSearch(radarRequest, function(results, status) {
+//       if (status == google.maps.places.PlacesServiceStatus.OK) {
+//         // for (var i = 0; i < length.results; i++) {
+//         for (var i = 0, result; result = results[i]; i++) {
+//           var marker = createMarker(result);
+//         }
+//       }
+//       //as long as we haven't triggered the query limit, add 1 to the index and search again
+//       if (status != google.maps.places.PlacesServiceStatus.OVER_QUERY_LIMIT) {
+//         searchIndex++;
+//         if (searchIndex < boxes.length)
+//           findPlaces(searchIndex);
+//       } else { // delay 1 second and try again
+//         setTimeout("findPlaces(" + searchIndex + ")", 1000);
+//       }
+
+//     });
+//   }
+
+
+//   //create markers on each returned place result
+//   function createMarker(place) {
+//     var placeLoc = place.geometry.location;
+
+//     var image = {
+//       url: "https://maps.gstatic.com/intl/en_us/mapfiles/markers2/measle.png",
+//       size: new google.maps.Size(7, 7),
+//       anchor: new google.maps.Point(3.5, 3.5)
+//     };
+
+//     var marker = new google.maps.Marker({
+//       map: map,
+//       icon: image,
+//       position: place.geometry.location
+//     });
+
+//     var infowindow = new google.maps.InfoWindow();
+
+//     google.maps.event.addListener(marker, 'click', function() {
+//       service.getDetails(place, function(result, status) {
+//         if (status !== google.maps.places.PlacesServiceStatus.OK) {
+//           console.error(status);
+//           return;
+//         }
+//       infowindow.setContent(result.name);
+//       infowindow.open(map, marker);
+//       });
+//     });
+
+//   }
+// }
+// initialize();
+
+
+
+//-------------------------------------------------------------------------
+//Search updated to Nearby and boxes closest to origin not included in search
+//--------------------------------------------------------------------------
+
+
+function initialize() {
+  // intial map hard coded to center at Oakland
+  var oakland = {lat: 37.8044, lng: -122.2711};
+
+  var mapOptions = {
+    center: oakland,
+    zoom: 8
+  };
+
+  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  var service = new google.maps.places.PlacesService(map);
+
+  //assigns variable routeBoxer as new object from RouteBoxer library
+  var routeBoxer = new RouteBoxer();
+
+  //directionsService makes a directions request from Google maps
+  //directionsRenderer has Google maps render the directions it just returned
+  var directionService = new google.maps.DirectionsService();
+  var directionsRenderer = new google.maps.DirectionsRenderer({
+    map: map
+  });
+
+  drawPath();
+
+  var boxes;
+
+  function drawPath() {
+
+    var distance = 5.0; // sets search box size to 5km from route path
+
+    //search parameters for draw path request: start point, end point, travel method
+    var pathRequest = {
+      origin: {lat: 37.8044, lng: -122.2711}, // hard coded to oakland for testing
+      destination: {lat: 38.2324, lng: -122.6367}, // hard coded to petaluma for testing
+      travelMode: google.maps.DirectionsTravelMode.DRIVING
+    };
+
+    // Make the directions request, if the status is ok, create a path and
+    // routeBox search boxes along the path
+    directionService.route(pathRequest, function(result, status) {
+      if (status == google.maps.DirectionsStatus.OK) {
+        directionsRenderer.setDirections(result);
+
+        //path_overview smooths out the path coordinates
+        var path = result.routes[0].overview_path;
+        // creates search boxes around the returned path
+        boxes = routeBoxer.box(path, distance);
+
+        //turn on drawBoxes for testing if you want to visualize search boundaries,
+        //function is commented out below
+        // drawBoxes();
+
+        //since direction request was successful,
+        //call findPlaces function with searchIndex zero
+        findPlaces(0);
+      } else {
+        alert("Directions query failed: " + status);
+      }
+    });
+
+  }
+
+  // Draw the array of boxes as polylines on the map - helpful to visualize search while testing
+  // function drawBoxes() {
+  //   var boxpolys = new Array(boxes.length);
+  //   for (var i = 0; i < boxes.length; i++) {
+  //     boxpolys[i] = new google.maps.Rectangle({
+  //       bounds: boxes[i],
+  //       fillOpacity: 0,
+  //       strokeOpacity: 1.0,
+  //       strokeColor: '#000000',
+  //       strokeWeight: 1,
+  //       map: map
+  //     });
+  //   }
+  // }
+
+
+  function findPlaces(searchIndex) {
+
+    // search request is defined as the area bound by each routeBox box,
+    // search type is hard coded as restaurant for testing
+    var radarRequest = {
+      bounds: boxes[searchIndex],
+      type: 'restaurant'
+    };
+
+    //radarSearch allows a 'type' search within a given radius,
+    // if search is successful, create a marker for each result.
+    // If not, set Timeout to allow a delay and search again due to query limits
+    service.nearbySearch(radarRequest, function(results, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        // for (var i = 0; i < length.results; i++) {
+        for (var i = 0, result; result = results[i]; i++) {
+          var marker = createMarker(result);
+        }
+      }
+      //as long as we haven't triggered the query limit, add 1 to the index and search again
+      if (status != google.maps.places.PlacesServiceStatus.OVER_QUERY_LIMIT) {
+        searchIndex++;
+        if (searchIndex < (boxes.length-2))
+          findPlaces(searchIndex);
+      } else { // delay 1 second and try again
+        setTimeout("findPlaces(" + searchIndex + ")", 1000);
+      }
+
+    });
+  }
+
+
+  //create markers on each returned place result
+  function createMarker(place) {
+    var placeLoc = place.geometry.location;
+
+    var image = {
+      url: "https://maps.gstatic.com/intl/en_us/mapfiles/markers2/measle.png",
+      size: new google.maps.Size(7, 7),
+      anchor: new google.maps.Point(3.5, 3.5)
+    };
+
+    var marker = new google.maps.Marker({
+      map: map,
+      icon: image,
+      position: place.geometry.location
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    google.maps.event.addListener(marker, 'click', function() {
+      service.getDetails(place, function(result, status) {
+        if (status !== google.maps.places.PlacesServiceStatus.OK) {
+          console.error(status);
+          return;
+        }
+      infowindow.setContent(result.name);
+      infowindow.open(map, marker);
+      });
+    });
+
+  }
+}
+initialize();
+
+
+
+//=============================================================================
+//---------------Brainstorming / this section doesn't work------------------------
+//==============================================================================
+
 //   //Creates path overlay on map from start to end location, hard coded for stinson beach to petaluma
   // function displayDirections() {
 
@@ -175,157 +518,6 @@
 //   //incorporate later when start and end are taken from DOM
 //   // var start = document.getElementById('start').value;
 //   // var end = document.getElementById('end').value;
-
-
-
-
-
-
-// --------------Full function to return search results along path------------------------
-
-
-
-function initialize() {
-  // intial map hard coded to center at Oakland
-  var oakland = {lat: 37.8044, lng: -122.2711};
-
-  var mapOptions = {
-    center: oakland,
-    zoom: 8
-  };
-
-  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-  var service = new google.maps.places.PlacesService(map);
-
-  //assigns variable routeBoxer as new object from RouteBoxer library
-  var routeBoxer = new RouteBoxer();
-
-  //directionsService makes a directions request from Google maps
-  //directionsRenderer has Google maps render the directions it just returned
-  var directionService = new google.maps.DirectionsService();
-  var directionsRenderer = new google.maps.DirectionsRenderer({
-    map: map
-  });
-
-  route();
-
-
-  var boxes;
-
-  function route() {
-
-    var distance = parseFloat(5); // sets search box size to 5km from route path
-
-    var request = {
-      origin: {lat: 37.8044, lng: -122.2711},
-      destination: {lat: 38.2324, lng: -122.6367},
-      travelMode: google.maps.DirectionsTravelMode.DRIVING
-    };
-
-    // Make the directions request, if the status is ok, create a path and boxes along the path
-    directionService.route(request, function(result, status) {
-      if (status == google.maps.DirectionsStatus.OK) {
-        directionsRenderer.setDirections(result);
-
-        //path_overview smooths out the path coordinates
-        var path = result.routes[0].overview_path;
-        // Box around the overview path of the first route,
-        boxes = routeBoxer.box(path, distance);
-
-        //since direction request was successful,
-        //call findPlaces function with searchIndex zero
-        findPlaces(0);
-      } else {
-        alert("Directions query failed: " + status);
-      }
-    });
-
-  }
-
-  // Draw the array of boxes as polylines on the map - helpful to visualize search while testing
-  // function drawBoxes() {
-  //   var boxpolys = new Array(boxes.length);
-  //   for (var i = 0; i < boxes.length; i++) {
-  //     boxpolys[i] = new google.maps.Rectangle({
-  //       bounds: boxes[i],
-  //       fillOpacity: 0,
-  //       strokeOpacity: 1.0,
-  //       strokeColor: '#000000',
-  //       strokeWeight: 1,
-  //       map: map
-  //     });
-  //   }
-  // }
-
-
-  function findPlaces(searchIndex) {
-
-    // search request is defined as the area bound by each routeBox box,
-    // search type is hard coded as restaurant for testing
-    var radarRequest = {
-      bounds: boxes[searchIndex],
-      type: 'restaurant'
-    };
-
-    //radarSearch allows a 'type' search within a given radius,
-    // if search is successful, create a marker for each result.
-    // If not, set Timeout to allow a delay and search again due to query limits
-    service.radarSearch(radarRequest, function(results, status) {
-      if (status == google.maps.places.PlacesServiceStatus.OK) {
-        // for (var i = 0; i < length.results; i++) {
-        for (var i = 0, result; result = results[i]; i++) {
-          var marker = createMarker(result);
-        }
-      }
-      //as long as we haven't triggered the query limit, add 1 to the index and search again
-      if (status != google.maps.places.PlacesServiceStatus.OVER_QUERY_LIMIT) {
-        searchIndex++;
-        if (searchIndex < boxes.length)
-          findPlaces(searchIndex);
-      } else { // delay 1 second and try again
-        setTimeout("findPlaces(" + searchIndex + ")", 1000);
-      }
-
-    });
-  }
-
-
-  //create markers on each returned place result
-  function createMarker(place) {
-    var placeLoc = place.geometry.location;
-
-    var image = {
-      url: "https://maps.gstatic.com/intl/en_us/mapfiles/markers2/measle.png",
-      size: new google.maps.Size(7, 7),
-      anchor: new google.maps.Point(3.5, 3.5)
-    };
-
-    var marker = new google.maps.Marker({
-      map: map,
-      icon: image,
-      position: place.geometry.location
-    });
-
-    var infowindow = new google.maps.InfoWindow();
-
-    google.maps.event.addListener(marker, 'click', function() {
-      service.getDetails(place, function(result, status) {
-        if (status !== google.maps.places.PlacesServiceStatus.OK) {
-          console.error(status);
-          return;
-        }
-      infowindow.setContent(result.name);
-      infowindow.open(map, marker);
-      });
-    });
-
-  }
-}
-initialize();
-
-
-
-//--------------------------------------------------------------------------
 
 
 
